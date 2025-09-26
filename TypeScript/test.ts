259b9f8ccd8428bbbefa3d939607fd7da381184f
Kevin Neyer <kevinneyer@gmail.com>
2025-09-26 16:58:06 -0400

    begin TypeScript algo

--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -1,1 +1,11 @@
-// Testing for compiler.
+function reverseString(s: string[]): void {
+    let i: number = 0;
+    let j: number = s.length - 1;
+    let sCopy: Array<string> = JSON.parse(JSON.stringify(s));
+
+    while (i < s.length) {
+        s[i] = sCopy[j];
+        i++;
+        j--;
+    }
+};
